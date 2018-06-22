c44f8dccdef8cb32a529c0dcb5724623f7db87ad
Richard Fenix <richard.fenix@gmail.com>
2018-06-22 14:11:06 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,32 @@
+module Memorable
+  
+  def reset_all
+    self.all.clear
+  end
+
+  def count
+    self.all.count
+  end
+  
+end
+
+  # Artist class code
+  
+  # def self.reset_all
+  #   self.all.clear
+  # end
+
+  # def self.count
+  #   @@artists.count
+  # end
+
+  # Song class code
+  
+  # def self.reset_all
+  #   @@songs.clear
+  # end
+
+  # def self.count
+  #   self.all.count
+  # end
+
