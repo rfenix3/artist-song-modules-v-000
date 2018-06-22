55b8654ac1ba53ceb278ae208f7eea7ab26d1b3d
Richard Fenix <richard.fenix@gmail.com>
2018-06-22 13:43:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,19 +21,20 @@ class Song
     @artist = artist
   end
 
-  # def self.reset_all
-  #   @@songs.clear
-  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  # def self.count
-  #   self.all.count
-  # end
-
   def self.all
     @@songs
   end
 end
+
+  # def self.count
+  #   self.all.count
+  # end
+
+  # def self.reset_all
+  #   @@songs.clear
+  # end
